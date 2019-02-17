775790349eabc19e369c7799ac7ea754313e1e61
pgiardiniere <pgiardiniere@gmail.com>
2019-02-16 23:43:59 -0500

    add notes

---
+++ 1.7-timeExec.py
@@ -0,0 +1,15 @@
+### Timing code execution
+# A handful of iPython magics useful for benchmarking your code's execution:
+# 
+# %time     Time exec of a single statement
+# %timeit   Time exec of a single statement w/ more accurracy 
+# %prun     Profile-Run --- Run code with the profiler
+# %lprun    Line-Profile-Run --- Run code with the line-by-line profiler
+# %memit    Measure memory use of a single statement
+# %mprun    Memory-Profile-Run --- Run code with line-by-line memory profiler
+
+# magics 3-6 are not included by default with iPython
+# 3-4 are part of the 'line-profiler' extension
+# 5-6 are part of the 'memory-profiler' extension
+
+
