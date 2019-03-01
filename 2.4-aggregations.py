7151d055f9e96964f241c17d1d9549bd6f603f37
pgiardiniere <pgiardiniere@gmail.com>
2019-02-28 22:50:32 -0500

    add notes, data sources, and gitignore

--- 2.4-aggregations.py
+++ 2.4-aggregations.py
@@ -28,12 +28,65 @@ np.min(big_array), np.max(big_array)
 # personally, find it marginally more readable
 big_array.min(), big_array.max(), big_array.sum()
 
-### Multi-dimensional aggregates
+## Multi-dimensional aggregates
 # in 2D arrays, we often want aggregates organized along row or column
 M = np.random.random((3, 4))
 M
 M.sum()         # returns sum of entire array
 
 # we can specify an additional arg 'axis' to specify where aggregation takes place
-M.min(axis=0)   # returns min of each corresponding column
-M.max(axis=1)   # returns max of each corresponding row
+# the axis keyword specifies which array dimension will be collapsed, NOT returned
+M.min(axis=0)   # returns min of each corresponding column (collapse along x)
+M.max(axis=1)   # returns max of each corresponding row    (collapse along y)
+
+
+## Other aggregation functions
+# NOTE most NumPy agg funcs have a NaN-safe alternative
+# np.sum() ~~ np.nansum()
+
+# the following (incomplete) list of aggregates will be touched frequently:
+np.sum
+np.prod
+np.mean
+np.std
+np.var          # compute variance (max-min)
+np.min
+np.max 
+np.argmin       # return index of min value
+np.argmax       # return index of max value
+np.median
+np.percentile
+np.any          # evaluate whether any elements are true
+np.all          # evaluate whether all elements are true
+
+
+## example: what is avg height of the US presidents
+# NOTE I copied over the 'data' directory from the book into notes dir, can complete example work
+
+# the following is only executable in iPython directly
+# !head -4 data/president_heights.csv       # or not... doesn't work there either
+import pandas as pd
+data = pd.read_csv('data/president_heights.csv')    # reads csv into memory
+heights = np.array(data['height(cm)'])              # takes 'heights' column from csv var
+print(heights)
+
+heights.mean()
+heights.std()
+heights.min()
+heights.max()
+
+np.percentile(heights, 25)
+np.median(heights)
+np.percentile(heights, 75)
+
+## we can also visualize with Matplotlib
+# again, another line which errors out below
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn; seaborn.set()   # set plot style
+plt.hist(heights)
+plt.title('Height Distribution of US Presidents')
+plt.xlabel('height (cm)')
+plt.ylabel('number')
+# not printing anything ... the above might be dependent on running in a Jupyter session
+
