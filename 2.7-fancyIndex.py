11cb271d211755a8a76684997237e73ff7d130f4
pgiardiniere <pgiardiniere@gmail.com>
2019-03-05 18:03:42 -0500

    add notes

--- 2.7-fancyIndex.py
+++ 2.7-fancyIndex.py
@@ -47,3 +47,42 @@ X[1:, [2, 0, 1]]                            # Slicing
 mask = np.array([1, 0, 1, 0], dtype=bool)
 X[row[:, np.newaxis], mask]                 # Masking
 
+### Example: selecting random points
+# Common use of fancy indexing: select subsets of rows in a matrix
+# Consider an arbitrary number of data points N for a data set with arbitrary number of dimsnesions D 
+# (in this case, a 2d normal distribution)
+mean = [0, 0]
+cov = [[1, 2],
+       [2, 5]]
+X = rand.multivariate_normal(mean, cov, 100)
+X.shape     # returns (100, 2)
+
+## once again, matplotlib materials. Haven't gotten to the chapter to properly configure this stuff
+import matplotlib.pyplot as plt
+import seaborn; seaborn.set() # for plot styling
+
+plt.scatter(X[:, 0], X[:, 1])
+
+# with fancy indexing, we can easily select 20 random points (ensuring that they're each unique)
+indeces = np.random.choice(X.shape[0], 20, replace=False)
+indeces
+selection = X[indeces]
+selection.shape
+
+# to see our selection on the graph, we can plot circles over them
+plt.scatter(X[:, 0], X[:, 1], alpha = 0.3)
+plt.scatter(selection[:, 0], selection[:, 1], facecolor='none', s=200) 
+
+### Modifying values with Fancy Indexing
+# In the following example, we use an array of indeces to modify the data at those indeces
+x = np.arange(10)
+i = np.array[2, 1, 8, 4]
+x[i] = 99
+x
+# we can use any assignment operators
+x[i] -= 10
+# be sure to track when using a single index for multiple operations, you may lose one of your vals
+x[[0, 0]] = [4, 6]  # assigns 4, then assigns 6 to index 0. Only 6 will remain
+x
+
+
