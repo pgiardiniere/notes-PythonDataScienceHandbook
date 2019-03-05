3902d673b1f58d1fc3698e58becc1459bda8fd1c
pgiardiniere <pgiardiniere@gmail.com>
2019-03-05 17:39:39 -0500

    add notes

--- 2.7-fancyIndex.py
+++ 2.7-fancyIndex.py
@@ -25,4 +25,25 @@ x[ind]
 X = np.arange(12).reshape((3, 4))
 X
 
-row = np.array([0, 1, 2])]
+row = np.array([0, 1, 2])
+col = np.array([2, 1, 3])
+X[row, col] 
+# values returned located at (0,2)(1,1)(2,3) - this is due to how the Broadcasting rules are applied
+# following that logic, we can apply principle to row/column vectors (in addition to the scalars per above)
+
+X[row[:, np.newaxis], col]
+# recall row[:, np.newaxis] will translate the existing array to a 2d array of 1 column
+# so now the return is a (broadcasted) 3x3 grid 
+
+# same thing with broadcasting, we can apply mathematical operations
+row[:, np.newaxis] * col
+
+
+### Combined indexing
+# if you use Fancy indexing for part of your access arg, you can still use other forms of access in conjunction with it
+# e.g. simple indexing, slicing, and masking
+X[2, [2, 0, 1]]                             # Simple Indexing
+X[1:, [2, 0, 1]]                            # Slicing
+mask = np.array([1, 0, 1, 0], dtype=bool)
+X[row[:, np.newaxis], mask]                 # Masking
+
