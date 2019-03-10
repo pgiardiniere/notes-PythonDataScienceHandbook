63de092b48485e9db7fee8fc9de357b5e39f6889
pgiardiniere <pgiardiniere@gmail.com>
2019-03-09 23:58:46 -0500

    add notes

--- 4.0-introMatplotlib.py
+++ 4.0-introMatplotlib.py
@@ -41,3 +41,59 @@ plt.plot(x, np.cos(x))
 plt.show()
 # if you run this script from command-line prompt, it will result in a window
 # opening with the figure --- yay it works
+
+####################
+### Plotting from an iPython shell
+# %matplotlib
+### Plotting from an iPython notebook
+# %matplotlib notebook  (interactive plots)
+# %matplotlib inline    (static images of plots)
+
+# generally will use inline. The following example will embed a png in the notebook
+import numpy as np
+x = np.linspace(0, 10, 100)
+
+fig = plt.figure()
+plt.plot(x, np.sin(x), '-')
+plt.plot(x, np.cos(x), '--')
+
+## Saving figures to file with savefig()
+fig.savefig('my_figure.png') 
+
+# can confirm with an 'ls -lh my_figure.png' - new file in current working dir
+
+# can also use python to display the contents of it using following:
+from IPython.display import Image
+Image('my_figure.png')
+
+# savefig() infers filetype from the extension you give the filename
+# Depending on backends installed, the file formats differ. Print list with:
+fig.canvas.get_supported_filetypes()
+
+####################
+### MATLAB dual-interface --- state-based vs object-based
+
+## State-based interface: MATLAB style
+# Recall that Matplotlib originated as a Python alternative for current MATLAB users
+# it's syntax often is more reflective of MATLAB than python for this reason
+
+# ---------- MATLAB style example ----------
+plt.figure()    # create plot figure
+
+# create the first of two panels and set current axis
+plt.subplot(2, 1, 1)    # (rows, columns, panel number)
+plt.plot(x, np.sin(x))
+
+# create the second panel and set current axis
+plt.subplot(2, 1, 2)
+plt.plot(x, np.cos(x))
+# ---------- End example --------------------
+
+# now we can discuss what 'stateful' means:
+# it's keeping track of the current figure and axes, which are where all 'plt'
+# commands are applied
+# you can get reference to these using 
+    # plt.gcf (get current figure) 
+    # plt.gca (get current axes)
+
+## Object-based interface: More power
