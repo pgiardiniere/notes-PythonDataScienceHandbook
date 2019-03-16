3ea857b364bfb65f3caea528fc34e47748076aae
pgiardiniere <pgiardiniere@gmail.com>
2019-03-15 21:04:20 -0400

    add notes

--- 4.6-plotLegends.py
+++ 4.6-plotLegends.py
@@ -33,3 +33,59 @@ ax.legend(fancybox=True, framalpha=1, shadow=True, borderpad=1)
 fig
 
 # see additional legend options by checking plt.legend docstring
+
+###################################
+### Choosing elements for the legend
+# as we have seen, legend includes all labeled elems by default
+# plt.plot() command is able to create multiple lines, returns a list of created line instances
+# passing any given line to plt.legend() tells it which to identify, and it's label:
+
+y = np.sin(x[:, np.newaxis] + np.pi * np.arange(0, 2, 0.5))
+lines = plt.plot(x, y)
+# lines is a list of plt.Line2D instances
+plt.legend(lines[:2], ['first', 'second'])
+
+# in practice, it is author's opinion that is clearer to use the first method,
+# i.e. apply labels to the plot elements you want to show in the legend in advance
+plt.plot(x, y[:, 0], label='first')
+plt.plot(x, y[:, 1], label='second')
+plt.plot(x, y[:, 2:])
+plt.legend(framalpha=1, framon=True)
+
+# I would agree, second example shows the labels at plt.plot() point,
+# so of course plt.legend() would pick those up and not the third
+
+### Legend for Size of Points
+# sometimes the defaults are not sufficient for a visualization
+    # HAH! HAHA! THE AUTHOR MADE A TYPO
+    # "For example, perhaps you're be using the size of points to mark..."
+    # he's human like me after all lel
+
+# following is an example of using size of points to indicate pops
+# legend should specify scale of sizes at points, accomplished by plotting labeled data with no entries
+
+# ------------- example begin -----------------
+import pandas as pd
+cities = pd.read_csv('data/california_cities.csv')
+
+# extract data we're analyzing
+lat, lon = cities['latd'], cities['longd']
+population, area = cities['population_total'], cities['area_total_km2']
+
+# Scatter the points, using size and color, but no label
+plt.scatter(lon, lat, label=None,
+            c=np.log10(population), cmap='viridis',
+            s=area, linewidth=0, alpha=0.5)
+plt.axis(aspect='equal')
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+plt.colorbar(label='log$_{10}$(population)')
+plt.clim(3, 7)
+
+# create legend & plot: plotting empty lists with desired size and label
+for area in [100, 300, 500]:
+    plt.scatter([], [], c='k', alpha=0.3, s=area, label=str(area) + ' km$^2$')
+plt.legend(scatterpoints=1, frameon=False, labelspacing=1, title='City Area')
+plt.title('California Cities: Area and Population')
+
+# ------------- example end -----------------
