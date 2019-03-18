6781ef5f111da5562d39ef178ec521d1ab00ef15
pgiardiniere <pgiardiniere@gmail.com>
2019-03-17 23:18:40 -0400

    add notes

--- 4.7-colorBars.py
+++ 4.7-colorBars.py
@@ -31,5 +31,43 @@ plt.imshow(I, cmap='gray')
 # 3 different kinds of colormaps discussed here:
 
     # Sequential colormaps
+        # made up of one continuous sequence of colors
+        # e.g. binary, viridis
     # Divergent colormaps
+        # usually contain 2 distinct colors, positive and negative devs from mean
+        # e.g. RdBu, PuOr
     # Qualitative colormaps
+        # mix colors with no particular sequence
+        # e.g. rainbow, jet
+
+# jet actually used to be default for matplotlib, despite the fact that 
+# qualitative colormaps are generally the least-helpful scheme due to non-uniform progression
+
+# converting jet colorbar to black and white demonstrates this concept:
+from matplotlib.colors import LinearSegmentedColormap
+
+def grayscale_cmap(cmap):
+    """Return a grayscale version of the given colormap"""
+    cmap = plt.cm.get_cmap(cmap)
+    colors = cmap(np.arange(cmap.N))
+
+    # convert RGBA to perceived grayscale Luminance
+    # cf. http://alienryderflex.com/hsp.html
+    RGB_weight = [0.299, 0.587, 0.114]
+    luminance = np.sqrt(np.dot(colorsp:, :3] ** 2, RGB_weight))
+    colors[:, :3] = luminance[:, np.newaxis]
+
+    return LinearSegmentedColormap.from_list(cmap.name + "_gray", colors, cmap.N)
+
+def view_colormap(cmap):
+    """Plot a colormap with its grayscale equivalent"""
+    cmap = plt.cm.get_cmap(cmap)
+    colors = cmap(np.arange(cmap.N))
+
+    cmap = grayscale_cmap(cmap)
+    grayscale = cmap(np.arange(cmap.N))
+
+    fig, ax = plt.subplots(2, figsize=(6, 2), 
+                           subplot_kw=dict(xticks=[], yticks=[]))
+    ax[0].imshow([colors], extent=[0, 10, 0, 1])
+    ax[1].imshow([grayscale], extent=[0, 10, 0, 1])
