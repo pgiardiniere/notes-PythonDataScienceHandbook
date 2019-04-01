fde63587436d47650b148edc4f5c52d9ce2f9a7e
pgiardiniere <pgiardiniere@gmail.com>
2019-03-31 23:58:06 -0400

    add notes

---
+++ 5.0-customizingTicks.py
@@ -0,0 +1,9 @@
+### major and minor ticks
+# setup
+
+import matplotlib.pyplot as plt
+plt.style.use('classic')
+# %matplotlib inline
+import numpy as np
+
+ax = plt.axes(xscale='log', yscale='log')
