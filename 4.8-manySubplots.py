40b3d648f2c854e4ec68135093c7db1c9aca5106
pgiardiniere <pgiardiniere@gmail.com>
2019-04-01 22:57:10 -0400

    add notes

--- 4.8-manySubplots.py
+++ 4.8-manySubplots.py
@@ -21,3 +21,46 @@ import numpy as np
 
 ax1 = plt.axes()    # standard axes
 ax2 = plt.axes([0.65, 0.65, 0.2, 0.2])
+
+# equivalent in the object-oriented interface: 
+    # fig.add_axes()
+# example, 2 vertically stacked axes
+fig = plt.figure()
+ax1 = fig.add_axes([0.1, 0.5, 0.8, 0.4], xticklabels=[], ylim=(-1.2, 1.2))
+ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.4], ylim=(-1.2, 1.2))
+
+x = np.linspace(0, 10)
+ax1.plot(np.sin(x))
+ax2.plot(np.cos(x))
+
+
+### plt.subplot() --- simple, singular grids of subplots
+# aligned columns/rows of subplots are common, so Matplotlib has built-in
+# methods for this use case
+
+# lowest level: creates a single subplot within a grid
+# plt.subplot() || fig.add_subplot() -- latter being object oriented version
+# takes 3 ints as args. num rows, num cols, index of plot to be created
+# (upper left to the bottom right)
+for i in range(1, 7):
+    plt.subplot(2, 3, i)
+    plt.text(0.5, 0.5, str((2, 3, i)), fontsize=18, ha='center')
+
+# plt.subplots_adjust(), unsurprisingly, adjusts spacing between plots
+# use OOP method below
+fig = plt.figure()
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
+for i in range(1, 7):
+    ax = fig.add_subplot(2, 3, i)
+    ax.text(0.5, 0.5, str((2, 3, i)), fontsize=18, ha='center')
+
+
+### plt.subplots() --- multiple grids of subplots (note the 's')
+# when dealing with larger grids of subplots,
+# especially when hiding x/y axis labels on inner plots, we use this method. 
+# function specifically creates a full grid of subplots in a line,
+# and returns in a NumPy array. Args are num rows and num cols
+# optional arg: sharex / sharey
+
+# example: create 2 x 3 grid of subplots, al
+fig, ax = plt.subplots(2, 3, sharex='col', sharey='row')
