d6670ee42bb91a3f264d45cb08fbfadb02a328c9
pgiardiniere <pgiardiniere@gmail.com>
2019-04-01 23:12:22 -0400

    add notes

--- 4.8-manySubplots.py
+++ 4.8-manySubplots.py
@@ -64,3 +64,57 @@ for i in range(1, 7):
 
 # example: create 2 x 3 grid of subplots, al
 fig, ax = plt.subplots(2, 3, sharex='col', sharey='row')
+
+# by specifying sharex/sharey, we inner labels are auto-removed.
+# because returned in NumPy array, can use standard array index notation 
+
+# axes are in a 2D array, indexed by [row, column]
+for i in range(2):
+    for j in range(3):
+        ax[i, j].text(0.5, 0.5, str((i, j)), fontsize=18, ha='center')
+
+# plt.subplot() indexes at 1
+# plt.subplots() indexes at 0   hurrah
+
+
+#########################
+### plt.GridSpec() for more complicated arrangements
+# plt.GridSpec() object does not create a plot by itself,
+# it is an interface recognized by the plt.subplot() cmd
+
+# example: gridspec for a grid of two rows and three columns
+grid = plt.GridSpec(2, 3, wspace=0.4, hspace=0.3)
+
+# then we can specify subplot location/extents with typical Python slicing syntax
+plt.subplot(grid[0, 0])
+plt.subplot(grid[0, 1:])
+plt.subplot(grid[1, :2])
+plt.subplot(grid[1, 2])
+
+# flexible grid alignment has many applications.
+# author likes it when doing multi-axes histogram plots such as below:
+# ----------begin----------------------------------------
+
+# create some normally distributed data
+mean = [0, 0]
+cov = [[1, 1], [1, 2]]
+x, y = np.random.multivariate_normal(mean, cov, 3000).T
+
+# set up axes with gridspec
+fig = plt.figure(figsize=(6, 6))
+grid = plt.GridSpec(4, 4, hspace=0.2, wspace=0.2)
+main_ax = fig.add_subplot(grid[:-1, 1:])
+y_hist = fig.add_subplot(grid[:1, 0], xticklabels=[], sharey=main_ax)
+x_hist = fig.add_subplot(grid[-1, 1:], yticklabels=[], sharex=main_ax)
+
+# scatter points on the main axes
+main_ax.plot(x, y, 'ok', markersize=3, alpha=0.2)
+
+# histogram on the attached axes
+x_hist.hist(x, 40, histtype='stepfilled', orientation='vertical', color='gray')
+x_hist.invert_yaxis()
+
+y_hist.hist(y, 40, histtype='stepfilled', orientation='horizontal', color='gray')
+y_hist.invert_xaxis()
+
+# -----------end-----------------------------------------
