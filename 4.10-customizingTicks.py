e311e1467f7bbc6e02e7b9681de8a679824bb1b3
pgiardiniere <pgiardiniere@gmail.com>
2019-04-03 23:48:19 -0400

    add notes

--- 4.10-customizingTicks.py
+++ 4.10-customizingTicks.py
@@ -34,3 +34,53 @@ print(ax.xaxis.get_minor_formatter())
 # by a 'LogLocator' (makes sense, it's a logarithmic plot)
 
 # as the minor ticks have a .NullFormatter, they do not have any labels.
+
+#########################
+### Hiding ticks or labels
+# can easily hide ticks/labels using
+# plt.NullLocator() and plt.NullFormatter()
+
+ax = plt.axes()
+ax.plot(np.random.rand(50))
+
+ax.yaxis.set_major_locator(plt.NullLocator())
+ax.xaxis.set_major_formattor(plt.NullFormattor())
+
+# X axis, removed labels but kept ticks
+# Y axis, removed labels and ticks
+    # really, we removed the Ticks from Y axis, which means there cannot be labels for null ticks
+
+# useful example: images of faces, something commonly used in supervised
+# machine learning problems
+
+fig, ax = plt.subplots(5, 5, figsize=(5, 5))
+fig.subplots_adjust(hspace=0, wspace=0)
+
+# Get some face data from scikit-learn
+from sklearn.datasets import fetch_olivetti_faces
+faces = fetch_olivetti_faces().images
+
+for i in range(5):
+    for j in range(5):
+        ax[i, j].xaxis.set_major_locator(plt.NullLocator())
+        ax[i, j].yaxis.set_major_locator(plt.NullLocator())
+        ax[i, j].imshow(faces[10 * i + j], cmap="bone")
+
+# notice each image has its own axes, with no locatorsm, as the tick values 
+# (i.e. pixel numbers) don't convey any info in our visualization
+
+#########################
+### Reducing or increasing the number of ticks
+# small subplots get crowded labels w/ default settings, e.g.:
+fix, ax = plt.subplots(4, 4, sharex=True, sharey=True)
+
+# numbers with near overlap = bad
+# use plt.MaxNLocator() to specify the max num ticks displayed
+# Matplotlib will use other internal logic to choose locations given max
+
+# for every axis, set the x and y major locator
+for axi in ax.flat:
+    axi.xaxis.set_major_locator(plt.MaxNLocator(3))
+    axi.yaxis.set_major_locator(plt.MaxNLocator(3))
+fig
+
