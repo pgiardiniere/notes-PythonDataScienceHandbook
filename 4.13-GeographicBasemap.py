9906bf4873a9137b63acedb60748160983d3194c
pgiardiniere <pgiardiniere@gmail.com>
2019-04-05 23:42:12 -0400

    add notes

---
+++ 4.13-GeographicBasemap.py
@@ -0,0 +1,22 @@
+# a common visualization is that of geographic data
+# Matplotlib's main tool is the Basemap toolkit
+# lives in the mpl_toolkits namespace
+# Basemap is kinda clunky, often simple visualization take long to create
+
+# The more modern counterparts (non-python) would be leaflet or Google Maps API
+
+# Still, it may be worth spending a few minutes to learn basic Basemap stuff
+
+# installation:
+# conda install basemap
+
+# import:
+# %matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+# seems like it has been retired then... or replaced. Interesting
+
+plt.figure(figsize=(8, 8))
+m = Basemap(projection='ortho', resolution=None, lat_0=50, lon_0=-100)
+m.bluemarble(scale=0.5)
