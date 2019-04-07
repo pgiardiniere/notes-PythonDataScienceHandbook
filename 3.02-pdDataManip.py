c965e5690622841533409e888a49db7bd74b69db
pgiardiniere <pgiardiniere@gmail.com>
2019-04-06 23:18:35 -0400

    add notes

---
+++ 3.02-pdDataManip.py
@@ -0,0 +1,5 @@
+# in prior, dove into NumPy and its ndarray object
+# now, we'll perform a similar exercise with Pandas
+
+# Recall Pandas is a newer package built on top of NumPy, 
+# providing an efficient implementation of a DataFrame
