d652e8359663909ef90b1d2e50513032d574573f
pgiardiniere <pgiardiniere@gmail.com>
2019-04-17 22:33:22 -0400

    add notes

--- 3.02-pdDataManip.py
+++ 3.02-pdDataManip.py
@@ -1,15 +1,55 @@
-# in prior, dove into NumPy and its ndarray object
-# now, we'll perform a similar exercise with Pandas
+# Recall CH 2, discussed methods to access/set/modify values in NP arrays
+# indexing, slicing, masking, fancy indexing, and combinations of them
+# arr[2, 1]   arr[:, 1:5]       arr[0, [1,5]]
 
-# Recall Pandas is a newer package built on top of NumPy, 
-# providing an efficient implementation of a DataFrame
+##############################
+### Data Selection in Series
+# As discussed, PD Series objects are similar to 1d NP arrays, and Python dicts
 
-#########################
-## oh, woops this is from the intro 3.00
+
+## Series as Dictionary
+# like dicts, object provides mapping from collection of keys to values
 import pandas as pd
-pd.__version__
+data = pd.Series([0.25, 0.5, 0.75, 1.0], index=['a', 'b', 'c', 'd'])
+data
+data['b']
 
-# get docstring
-    # pd.<TAB>
-    # pd?
-#########################
+# can also use dict-like Python expressins/methods to examine keys/indices = vals
+'a' in data         # evals to "True"
+data.keys()         # lists keys + dtype
+list(data.items())  # lists key-value mappings in total
+
+# Series objects can be modified with dict-like syntax
+# like you can extend dicts by assigning to a new key,
+#          can extend Series by assigning a new index value
+data['e'] = 1.25
+data
+
+
+## Series as a one-dimensional array
+# Series builds on dict-like interface and provides array-style item selection
+# like slices, masking, and fancy indexing. Each following:
+
+# slicing by explicit index
+data['a':'c']
+
+# slicing by implicit integer index
+data[0:2]
+
+# masking
+data[(data > 0.3) & (data < 0.8)]
+
+# fancy indexing
+data[['a', 'e']]
+
+# NOTE: when splicing with explicit index, the final index is INCLUSIVE
+#       when splicing with implicit index, the final index is EXCLUSIVE
+
+
+## Indexers: loc, iloc, ix
+# Slicing and indexing conventions are weird
+data[1]     # uses explicit indices     - INCLUSIVE
+data[1:3]   # uses implicit indices     - EXCLUSIVE
+
+data = pd.Series(['a', 'b', 'c'], index=[1, 3, 5])
+data
