87bd06c954739dba7c516832b30cd13553f733ae
pgiardiniere <pgiardiniere@gmail.com>
2019-04-20 10:32:54 -0400

    add notes

--- 3.04-missingData.py
+++ 3.04-missingData.py
@@ -32,10 +32,72 @@
 
 ## None: Pythonic missing data
 # None is a Python singleton object used for missing data in Python code.
-# Because "None" is a Python objects, it must be used in an array of dtype "object"
+# Because "None" is a Python object, must be in arr of data type "object"
 
 import numpy as np
 import pandas as pd
 
 vals1 = np.array([1, None, 3, 4])
-vals1
+vals1       # dtype="object"
+
+# per the given input data, the most fitting common type representation
+# NP inferred is Python Objects
+
+# unfortunately, this means we can't use vectorized ops, everything must be done at Python level
+# see ex. for timing diff in "Object" vs "int" NP arr summing
+for dtype in ['object', 'int']:
+    print("dtype =", dtype)
+    %timeit np.arange(1E6, dtype=dtype).sum()
+    print()
+
+# also, inclusion of Python objects means aggregations often produce errors
+vals1.sum()     # type error. Undefined result summing int and "None"
+
+
+## NaN: Numerical missing data
+# NaN (acronym for "Not a Number") is a special float-point value
+# recognized by all systems using standard IEEE float-point representation
+vals2 = np.array([1, np.nan, 3, 4])
+vals2.dtype     # dtype="float64"
+
+# ANY operation performed with a NaN float will be a new NaN
+1 + np.nan
+0 * np.nan
+
+# so now, aggregates over vals are well defined but not always useful
+vals2.sum(), vals2.min(), vals2.max()   # all NaN
+
+# can use special aggregations to ignore missing values
+np.nansum(vals2), np.nanmin(vals2), np.nanmax(vals2)
+
+# NOTE: there is NO NaN val for ints, strings, or other types
+
+
+## "NaN" and "None" in Pandas
+# Pandas is built to handle both interchangeable, converting b/w when needed
+pd.Series([1, np.nan, 2, None])     # makes "None" "NaN"
+
+# PD attempts to upcast type without available sentinel value if "NA" vals present
+# example: Ints > Floats
+x = pd.Series(range(2), dtype=int)
+x               # dtype "int32"
+x[0] = None     # converted to "NaN"
+x               # dtype "float64"
+
+# ---------------------------
+# Pandas upcasting conventions:
+# Typeclass     Conversion when storing NAs     NA sentinel value
+
+# floating      No change                       np.nan
+# object        No change                       None or np.nan
+# integer       Cast to float64                 np.nan
+# boolean       Cast to object                  None or np.nan
+
+# ---------------------------
+
+# NOTE: in Pandas, strings are always stored with "object" dtype
+
+
+##############################
+### Operating on Null Values
+# 
