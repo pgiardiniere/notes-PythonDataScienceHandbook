149917e497bcfcd4e97a3c93a7c932457a0f6c3a
pgiardiniere <pgiardiniere@gmail.com>
2019-04-21 22:26:13 -0400

    add notes

--- 3.05-hierarchalIndexing.py
+++ 3.05-hierarchalIndexing.py
@@ -34,10 +34,49 @@ pop[('California', 2010):('Texas', 2000)]
 pop[[i for i in pop.index if[1] == 2010]]
 
 
-
 ## ---------- The better way ----------
 # Use Pandas Multindex object - essentially an extension of the tuple
 # with pre-built operations to keep code clean as efficient as possible
 
 index = pd.MultiIndex.from_tuples(index)
 index
+
+# from output, can observe multiple levels of indexing 
+# state names and years in this case, and multiple labels for each data point
+
+# can re-index the series with this MultiIndex to see hierarchal rep. of data
+pop = pop.reindex(index)
+pop
+
+# first 2 columns of series show multiple index values
+# third column shows the data
+# first column blank entries are repetitions of the prior (hence, hierarchal)
+
+# now we can use familiar Pandas slicing notation:
+pop[:, 2010]
+
+
+## MultiIndex as extra dimension
+# Could easily have stored the data in above example using DataFrame
+# Can actually morph it by using unstack()
+pop_df = pop.unstack()
+pop_df
+
+# stack() does the opposite:
+pop_df.stack()
+
+# with MultiIndexing, can go beyond this trivial example to store 3+ dimensions
+# in both Series and DataFrame objects
+
+# for example, may want demographic data foreach state population
+# with multiindex, its simple as just adding another column to the DF
+pop_df = pd.DataFrame({'total': pop,
+                       'under18': [9267089, 9284094,
+                                   4687374, 4318033,
+                                   5906301, 6879014]})
+pop_df
+
+# also, ufuncs still work with MultiIndexed data
+# compute fraction of people under 18 by year:
+f_u18 = pop_df['under18'] / pop_df['total']
+f_u18.unstack()     # unstack() for formatting. neat!
