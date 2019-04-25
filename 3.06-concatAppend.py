01095aa5c4ef5ea8c0ce10320087134d774aa2fc
pgiardiniere <pgiardiniere@gmail.com>
2019-04-24 23:49:46 -0400

    add notes

--- 3.06-concatAppend.py
+++ 3.06-concatAppend.py
@@ -14,3 +14,31 @@ def make_df(cols, ind):
 
 # example DataFrame:
 make_df('ABC', range(3))
+
+# also create a class to allow display of multiple DataFrames side-by-side
+# it utilized the _repr_html_ method, iPython uses it to implement rich obj disp
+class display(object):
+    """Display HTML representation of multiple objects"""
+    template = """<div style="float: left; padding: 10px;">
+    <p style='font-family:"Courier New", Courier, monospace'>{0}</p>{1}
+    </div>"""
+    def __init__(self, *args):
+        self.args = args
+
+    def _repr_html_(self):
+        return '\n'.join(self.template.format(a, eval(a)._repr_html_())
+                         for a in self.args)
+    
+    def __repr(self):
+        return '\n\n'.join(a + '\n' + repr(eval(a))
+                           for a in self.args)
+
+
+##############################
+### Recall: Concatenation of NumPy arrays
+# Concatenation of Series/DF is similar to NP arrs, 
+# done via np.concatenate function discussed in "basics of NP arrays" ch
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+np.concatenate([x, y, z])
