41476f61110cd73d08d30234762f84e93d086642
pgiardiniere <pgiardiniere@gmail.com>
2019-04-26 15:10:59 -0400

    add notes

--- 3.06-concatAppend.py
+++ 3.06-concatAppend.py
@@ -42,3 +42,74 @@ x = [1, 2, 3]
 y = [4, 5, 6]
 z = [7, 8, 9]
 np.concatenate([x, y, z])
+
+# can also concatenate along a specified axis for multi-dimensional data:
+x = [[1, 2],
+     [3, 4]]
+np.concatenate([x, x], axis=1)
+
+
+##############################
+### Now: Concatenation of Pandas objects
+# pd.concat() executes similar syntax, and contains additional options:
+
+# Signature in Pandas v0.18
+pd.concat(objs, axis=0, join='outer', join_axes=None, ignore_index=False,
+          keys=None, levels=None, names=None, verify_integrity=False,
+          copy=True)
+
+# Again, simplest use-case is concatenation of just Series of DataFrame objs
+# Series:
+ser1 = pd.Series(['A', 'B', "C"], index=[1, 2, 3])
+ser2 = pd.Series(['D', 'E', 'F'], index=[4, 5, 6])
+pd.concat([ser1, ser2])
+# DataFrame:
+df1 = make_df('AB', [1, 2])
+df2 = make_df('AB', [3, 4])
+display('df1', 'df2', 'pd.concat([df1, df2])')  # as thought, no display produced
+
+# By default, DataFrames concatenate row-wise (axis=0)
+# can specify axis as well:
+df3 = make_df('AB', [0, 1])
+df4 = make_df('CD', [0, 1])
+display('df3', 'df4', "pd.concat([df3, df4], axis='col')")  # or axis=1
+
+
+### Duplicate Indices:
+# unlike np.concatenate, pd.concat preserves indices, even if duplicates
+x = make_df('AB', [0, 1])
+y = make_df('AB', [2, 3])
+y.index = x.index           # make duplicate indices
+display ('x', 'y', 'pd.concat([x, y])')
+
+# Can choose to handle such duplicates a few ways:
+# --------------------------------------------------
+## Option 1: Catch repeats as an error
+# T/F verify indices in result do not overlap, specify "verify_integrity" flag
+# Set to True, it will throw an error if indices overlap
+try:
+    pd.concat([x, y], verify_integrity=True)
+except ValueError as e:
+    print("ValueError:", e)
+
+## Option 2: Ignore the index
+# If the index itself doesn't matter, specify "ignore_index" flag
+# Set to True, it will create new integer index for resulting Series:
+display('x', 'y', 'pd.concat([x, y], ignore_index=True)')
+
+## option 3: Add MultiIndex keys
+# To make a hierarchally indexed Series/DataFrame, specify "keys" option 
+# Must add desired keys as args.    This circumvents duplicate indices by adding layer of abstraction
+display('x', 'y', "pd.concat([x, y], keys=['x', 'y'])")
+# --------------------------------------------------
+
+
+### Concatenation with Joins
+# In examples above, concatenated DFs with shared col names
+# If col names are disparate/varied, pd.concat includes methods to join
+
+# create DF with SOME shared columns:
+df5 = make_df('ABC', [1, 2])
+df6 = make_df('BCD', [3, 4])
+display('df5', 'df6', 'pd.concat([df5, df6]')
+
