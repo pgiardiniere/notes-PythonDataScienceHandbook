e9f19eeabb4ad8f1c55bc67268d276bf946d651e
pgiardiniere <pgiardiniere@gmail.com>
2019-05-06 16:07:05 -0400

    add notes

--- 3.10-vectorizedStringOps.py
+++ 3.10-vectorizedStringOps.py
@@ -24,14 +24,43 @@ names.str.capitalize()  # tab-complete str<> to list ALL vectorized str methods
 monte = pd.Series(['Graham Chapman', 'John Cleese', 'Terry Gilliam',
                    'Eric Idle', 'Terry Jones', 'Michael Palin'])
 
-# many Pandas vectorized string methods are mirrored by overall Python str methods
+
 ## List of PD Methods similar to Python string methods:
-# len()
-# ljust()
-# rjust()
-#
-#
-#
-#
-#
-#
+    # many Pandas vectorized string methods are mirrored by overall
+    # Python str methods
+
+    # len()     lower()
+    # ljust()   upper()
+    # rjust()   find()
+    # center()  rfind()
+    # etc... see ch for full list. most are self-explanatory
+
+# str.lower()       returns series of strings:
+monte.str.lower()
+# str.len()         returns length of string in Int:
+monte.str.len()
+# str.startswith()  returns Bool
+monte.str.startswith('T')
+# str.split()       returns list for each element
+monte.str.split()
+
+
+## Methods using regular expressions
+    # these methods accept regular expressions, and mirror API conventions 
+    # of Python's built-in "re" module
+
+# Method        Description
+# -------------------------
+match()         # call re.match() on each element.      return Bool
+extract()       # call re.match() on each element.      return match strs
+findall()       # call re.findall() on each el.
+replace()       # replace occurrences of pattern with other str
+contains()      # call re.search() on each el.          return bool
+count()         # count occurrences of pattern
+split()         # equiv. to str.split(), but with regexs
+rsplit()        # equiv. to str.rsplit, but with regexs
+# -------------------------
+
+# as always, regexs are quite powerful operators when dealing with strings:
+# get first name from each element:
+monte.str.extract('([A-Za-z]+)', expand=False)
