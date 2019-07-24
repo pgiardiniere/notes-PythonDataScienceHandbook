d7a3a310607b69b1280886462074157e7f316219
pgiardiniere <pgiardiniere@gmail.com>
2019-07-23 21:28:31 -0400

    add notes

--- 5.02-SciKit-Learn.py
+++ 5.02-SciKit-Learn.py
@@ -31,3 +31,41 @@ iris.head()
 %matplotlib inline
 import seaborn as sns; sns.set()
 sns.pairplot(iris, hue='species', size=1.5)
+# For use in Scikit-Learn, we will extract features matrix and target arr from DF
+
+X_iris = iris.drop('species', axis=1)
+X_iris.shape
+
+y_iris = iris['species']
+y_iris.shape
+# now we have properly formatted data to use.
+
+### Scikit-Learn Estimator API
+# See the Scikit-Learn API paper (linked below) for details on general rules
+# http://arxiv.org/abs/1309.0238
+
+## basics of the API
+# 
+
+## Supervised learning example: Simple Linear Regression
+# Fitting a line to (x, y) data
+import matplotlib.pyplot as plt 
+import numpy as np
+
+rng = np.random.RandomState(42)
+x = 10 * rng.rand(50)
+y = 2 * x - 1 + rng.randn(50)
+plt.scatter(x, y)
+
+# Now, with some data in place, can walk through fitting line
+
+##   1) Choose a class of model
+# Recall, every class of model is represented by a Python class.
+# General list of Regresssion models: http://scikit-learn.org/stable/modules/linear_model.html
+
+# for our linear regression model, import LinearRegression class:
+from sklearn.linear_model import LinearRegression
+
+##  2) Choose model hyperparameters
+model = LinearRegression(fit_intercept=True)
+model
