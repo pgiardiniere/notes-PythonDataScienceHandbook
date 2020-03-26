2736e05fd58b59e22d877e8dceb20013d293782d
pgiardiniere <pgiardiniere@gmail.com>
2020-03-26 00:29:41 -0400

    2.3 ufuncs revised

--- 2.3-npUFuncs.py
+++ 2.3-npUFuncs.py
@@ -16,20 +16,20 @@ values = np.random.randint(1, 10, size=5)
 # %timeit compute_reciprocals(values)
 
 # now, consider a case with a much bigger array of values:
-big_array = np.random.randint(1, 100, size=1000000)
+big_arr = np.random.randint(1, 100, size=1000000)
 # %timeit compute_reciprocals(big_array)
 
-# check the vals in iPython, note that it's orders of magnitude slower than it should be in both cases
+# note that both ops are way slower than they should be
 
 ##############################
 ### UFuncs introduction
 # Interface for statically typed, compiled routines are known as vectorized operations
 # can be achieved by performing an operation on the array, which then is applied to each el
-# Advantage of vectorized approach is it pushes loop into the compiled layer under NumPy, for faster exec
+# Advantage of vectorized approach is it pushes loop into the compiled layer, rather than being interpreted piecewise
 
 # sample 1: ufunc operation b/w Scalar type and Array
-compute_reciprocals(values)
-1.0 / values
+compute_reciprocals(big_arr)
+1.0 / big_arr
 # %timeit (1.0 / big_array)
 
 # sample 2: ufunc operation b/w Array type and Array
@@ -39,8 +39,7 @@ np.arange(5) / np.arange(1, 6)
 x = np.arange(9).reshape((3,3))
 2 ** x
 
-# as a general rule, vectorized computations are magnitudes faster than Python loops
-# try to replace loops with vectorized expressions like the above when possible
+# the Pythonic way :: replace loops with vectorized expressions like the above when possible
 
 
 ##############################
@@ -48,8 +47,7 @@ x = np.arange(9).reshape((3,3))
 # Two types, Unary ufuncs and Binary ufuncs
 
 ## Array Arithmetic
-# NumPy ufuncs are nice, they make use of Python's native arithmetic operators
-# addition, subtraction, multiplication, division, etc.
+# NumPy ufuncs use simple arithmetic operators
 x = np.arange(4)
 x
 x + 5
@@ -57,33 +55,35 @@ x - 5
 x * 2
 x / 2
 x // 2  # floor division 
--x      # unary negation
-x ** 2  # exponentiation
-x % 2   # modulus (remainder)
+-x
+x ** 2
+x % 2
 
 # can also string together operations, standard operations order respected
 -(0.5*x + 1) ** 2
 
-# the arithmetic operations are convenient wrappers for specific NP funcs, + relates to add
+# Arithmetic operators in this context are wrappers for specific NP funcs. I.e. + evaluates to add()
 np.add(x, 2)    # long form
 x + 2           # wrapper equivalent
 
-# can find a partial list of the long-form uFunc arithmetic utilies used above in the chapter text
+
+# We also have Boolean and Bitwise operators translated to NP ufuncs. Covered in "2.6-boolMasking.py"
 
 
 ## Absolute Value
-# python has built-in abs value func, np does recognize it
+# np plays nice with python standard library absolute val function abs()
 x = np.array([-2, -1, 0, 1, 2])
 abs(x)
-# np also has its own function np.absolute() --- also callable as np.abs()
+# np has its own ufunc np.absolute() --- also callable as np.abs()
 np.absolute(x)
 np.abs(x)
-# ufun also handles complex data, where abs val returns magnitude
-x = np.array([3 - 4j, 4 - 3j, 2 + 0j, 0 + 1j])
+
+x = np.array([3 - 4j, 4 - 3j, 2 + 0j, 0 + 1j])  # complex nums example
+abs(x)
 np.abs(x)
 
 
-## Trigonometric functions
+## Trigonometric ufuncs
 # we can start by defining an array of angles
 theta = np.linspace(0, np.pi, 3)
 theta
@@ -96,35 +96,49 @@ np.arcsin(x)
 np.arccos(x)
 np.arctan(x)
 
+# side-note: recall that doubles get 15 decimal digits of precision.
+#            to get less messy output, must round.
+#            NON-Ufunc:  np.round(np.sin(theta), decimals=15)  # rounds to 15th (dec precision limit)
+#                ufunc:  np.rint( np.sin(theta))              # rounds to 0 th decimal
+
+
 ## Exponents and logarithms
-x = [1, 2, 3]
-x
-np.exp(x)       # basic exp yields natural exponential: 2.718
-np.exp2(x)      
+import math;
+x = np.array([1, 2, 3])
+
+math.e ** x
+np.exp(x)
+
+2 ** x
+np.exp2(x)
+
+3 ** x
 np.power(3, x)
 
-x = [1, 2, 4, 10]
-x
-np.log(x)       # natural logarithm
-np.log2(x)      # base 2 logarithm
-np.log10(x)     # base 10 logarithm
-# specialized log funcs, useful for precision over small inputs
+x = [1, 2, 4, 10]           # NOTE: x is a list --> cannot use ufunc operators like ** or +
+
+np.log(x)                   # natural logarithm
+np.log2(x)                  # base 2 logarithm
+np.log10(x)                 # base 10 logarithm
+
+# related exp/log funcs, good for precision on small input vals
 x = [0, .001, .01, .1]
-np.expm1(x)
-np.log1p(x)
+np.exp(x)
+np.expm1(x)             # Calculates ``exp(x) - 1``  
+np.log(x)
+np.log1p(x)             # Calculates ``log(1 + x)``
 
 ## Specialized ufuncs
 # some additional examples: hyperbolic trig funcs, bitwise arithmetic, comparison operators,
-# radian > degree conversions, rounding/remainders, etc. See all in NumPy docs
+# radian > degree conversions, rounding/remainders, etc. See NumPy ufunc docs
 
-# another source (with demonstrations): 
-# scipy.special
+# another source of ufuncs :: scipy.special 
 from scipy import special
 
 # Gamma functions (generalized factorials) & related funcs
 x = [1, 5, 10]
 special.gamma(x)    # gamma
-special.gammaln(x)  # ln|gamma      not sure why pylint IDs as errors - runs fine
+special.gammaln(x)  # ln|gamma
 special.beta(x, 2)  # beta
 
 # Error function (integral of Gaussian), complement, and inverse
@@ -142,24 +156,25 @@ special.erfinv(x)
 x = np.arange(5)
 y = np.empty(5)
 np.multiply(x, 10, out=y)
-y
+
 # Can also utilize this with array views
 y = np.zeros(10)
 y = np.power(2, x, out=y[::2])
-y
-# compare speed between statement 2 and below, for a larger op, to see savings of not having temp array creation & copy
-# y[::2] = 2 ** x
+y[::2] = 2 ** x
+
+# the above 2 lines do the same thing, but 2nd line requires creation of temp array and full copy
+# so generally, specifying 'out' is just saying "store result in this memory location" in imperative style
+# means efficiency savings.
 
 
 ## Aggregates
-# With reduce() method, we can repeatedly execute a given operation to 
-# all elements of the array, until a single result remains
-# example with add, multiply
+# With reduce() method, we can repeatedly execute a given operation to all elements of the array
+# returning a single scalar value
 x = np.arange(1, 6)
 np.add.reduce(x)
 np.multiply.reduce(x)
 
-# with accumulate() method, we can reduce AND store all the intermediate values
+# with accumulate() method, we reduce() while storing all intermediate outputs in array
 np.add.accumulate(x)
 np.multiply.accumulate(x)
 
