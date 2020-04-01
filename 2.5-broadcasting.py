8dce51ee01ae0a6d266d2591903c3588b86baa4d
pgiardiniere <pgiardiniere@gmail.com>
2020-04-01 13:34:43 -0400

    revisions

--- 2.5-broadcasting.py
+++ 2.5-broadcasting.py
@@ -38,84 +38,79 @@ a = np.arange(3)
 
 M.shape     # returns (3, 3)
 a.shape     # returns (3,)
+M + a
 
-## by rule 1, a has fewer dimensions, so begin padding on left with ones:
+# by rule 1, a has fewer dimensions, so begin padding on left with ones:
 # M.shape --> (2, 3)
 # a.shape --> (1, 3)
 
-## by rule 2, 'a' has a first dimension which disagrees, so we stretch it to match
+# by rule 2, 'a' has a first dimension which disagrees, so it is stretched
 # M.shape --> (2, 3)
 # a.shape --> (2, 3)
 
-## now that shapes match, the operation is finally performed
-M + a   # returns: ([[1., 2., 3.],
-        #            [1., 2., 3.]]) 
+# Operation: ([[1., 2., 3.],
+#              [1., 2., 3.]])
 
-# ------------------------------
-### Broadcasting ex2:
+
+# Broadcasting ex2:
 a = np.arange(3).reshape((3, 1))
 b = np.arange(3)
 
 a.shape     # returns (3, 1)
 b.shape     # returns (3,)
 
-## by Rule 1, we pad shape of b with ones (on the left!):
+# by Rule 1, we pad shape of b with ones (on the left!):
 # a.shape --> (3, 1)
 # b.shape --> (1, 3)
 
-## by Rule 2, we upgrade each of these ones to match the corresponding size of the other:
+# by Rule 2, we upgrade each of these ones to match the corresponding size:
 # a.shape --> (3, 3)
 # b.shape --> (3, 3)
 
-## the result matches, and shapes are compatible. operation can run without error
+# The result matches, and shapes are compatible.
 a + b
 
-# ------------------------------
-### Broadcasting ex3: (demonstrates incompatible shapes, rule 3)
+
+# Broadcasting ex3: (demonstrates incompatible shapes, rule 3)
 M = np.ones((3, 2))
 a = np.arange(3)
 
 M.shape     # returns: (3, 2)
 a.shape     # returns: (3,)
 
-## by Rule 1, we pad shape of a with ones:
+# by Rule 1, we pad shape of a with ones:
 # M.shape --> (3, 2)
 # a.shape --> (1, 3)
 
-## by Rule 2, first dimension of 'a' is stretched to match M
+# by Rule 2, first dimension of 'a' is stretched to match M
 # M.shape --> (3, 2)
 # a.shape --> (3, 3)
 
 # But we can't stretch M, as the dimension which doesn't match is != 1
-# if the rules allowed stretching arrays in that way there would be amgiguous cases
-# so, we hit rule 3, "operands could not be broadcast together with shapes (3,2) (3,)"
-# M + A
 
-# in this particular case, we could make 'a' and M compatible by adding a's shape
-# with ones on the RIGHT, rather than left. We can pad on the right manually by adding a newaxis:
-a[:, np.newaxis].shape  # returns (3, 1)
+
+# in this case, we could define an add for 'a' and M by padding a's shape
+# with ones on the RIGHT, rather than left.
+a[:, np.newaxis].shape
 M + a[:, np.newaxis]
 
-# note this applies to all ufuncs, not just addition. used only for simplicity.
+# NOTE: broadcasting behavior applies to all ufuncs, not just addition.
 
 
-##############################
-### Broadcasting in Practice
+# Broadcasting in Practice:
 
-## Centering an array:
-# say you have an array of 10 observations, each containing 3 values
+
+# Centering an Array:
+# Center an array of 10 observations, each containing 3 values.
 X = np.random.random((10, 3))
-X
-# compute the mean using 'mean' aggregate across first dimension
-Xmean = X.mean(0)
-# center the x array by subtracting the mean
+
+Xmean = X.mean(0)       # Compute mean on first axis
 X_centered = X - Xmean
-# as a double check, by taking mean of centered array, it should be near-0
-X_centered.mean(0)
+X_centered.mean(0)      # Centered mean ~~= 0
 
 
-## Plotting a 2d function
-# can display images based on two-dimensional functions
+# Plotting a 2D Function:
+# We can display images based on two-dimensional functions.
 x = np.linspace(0, 5, 50)
 y = np.linspace(0, 5, 50)[:, np.newaxis]
 
