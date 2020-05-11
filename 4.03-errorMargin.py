441f13ff276ad0fc690fca69c3cbb7fb0e4a0246
pgiardiniere <pgiardiniere@gmail.com>
2020-05-10 23:42:56 -0400

    revisions

--- 4.03-errorMargin.py
+++ 4.03-errorMargin.py
@@ -1,12 +1,14 @@
-# often, to make accurrate comparisons of averages/data aggregates between disparate
-# data sets, we need to include Margin of Error information
+"""To make comparisons of averages/data aggregates on disparate data sets,
+use a Margin of Error.
+"""
 
-##############################
-### Basic errorbars in Matplotlib
-# %matplotlib inline
+# Basic errorbars in Matplotlib
+
+# Notebook setup:
+# %matplotlib inline`
 import matplotlib.pyplot as plt
-plt.style.use('seaborn-whitegrid')
 import numpy as np
+plt.style.use('seaborn-whitegrid')
 
 x = np.linspace(0, 10, 50)
 dy = 0.8
@@ -23,20 +25,20 @@ plt.errorbar(x, y, yerr=dy, fmt='o', color='black',
 
 # check errorbar docstring for more options
 
-##############################
-### Continuous errors
-# sometimes we want to show errorbars on continuous quantities
-# matplotlib has no built-in function, but can combine primitives for it
 
-## e.g. Gaussian process regression w/ Scikit-Learn API
+# Continuous errors:
+"""Sometimes we want to show errorbars on continuous quantities.
+matplotlib has no built-in function, but can combine primitives for it
+"""
+# e.g. Gaussian process regression w/ Scikit-Learn API
 from sklearn.gaussian_process import GaussianProcess
 
-# define model and draw data
+# Define a model and draw data.
 model = lambda x: x * np.sin(x)
 xdata = np.array([1, 3, 5, 6, 8])
 ydata = model(xdata)
 
-# compute the Gaussian process fit
+# Compute the Gaussian process fit.
 gp = GaussianProcess(corr='cubic', theta0=1e-2, thetaL=1e-4, thetaU=1E-1, random_start=100)
 gp.fit(xdata[:, np.newaxis], ydata)
 
@@ -44,10 +46,10 @@ xfit = np.linspace(0, 10, 1000)
 yfit, MSE = gp.predict(xfit[:, np.newaxis], eval_MSE=True)
 dyfit = 2 * np.sqrt(MSE)    # 2*sigma ~ 95% confidence region
 
-# to compute continuous error, we would not use plt.errorbar, as we don't want
-# 1000 points with 1000 errorars
+# To compute continuous error, we would not use plt.errorbar, as we don't want
+# 1000 points with 1000 errorbars.
 
-# instead, use plt.fill_between() with a light color to visualize it
+# Instead, use plt.fill_between() with a light color to visualize it
 # Visualize the result:
 plt.plot(xdata, ydata, 'or')
 plt.plot(xfit, yfit, '-', color='gray')
@@ -55,6 +57,7 @@ plt.plot(xfit, yfit, '-', color='gray')
 plt.fill_between(xfit, yfit - dyfit, yfit + dyfit, color='gray', alpha=0.2)
 plt.xlim(0, 10)
 
-# for more options, see plt.fill_between() docstring
-# this is a tad low-level, so in another section (visualization with Seaborn)
-# a streamlined API is discussed for visualizing this type of continuous errorbar
+# For more options, see plt.fill_between() docstring.
+
+# This is a tad low-level, so in the "Visualization With Seaborn" section,
+# a dedicated API is discussed for visualizing this type of continuous errorbar
