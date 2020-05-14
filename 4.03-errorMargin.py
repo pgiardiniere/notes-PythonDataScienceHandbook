9ab0466bee364d5523e87298b3765ac1fc3e5b30
pgiardiniere <pgiardiniere@gmail.com>
2020-05-13 23:42:54 -0400

    revisions, package no longer contains required module

--- 4.03-errorMargin.py
+++ 4.03-errorMargin.py
@@ -5,7 +5,7 @@ use a Margin of Error.
 # Basic errorbars in Matplotlib
 
 # Notebook setup:
-# %matplotlib inline`
+# %matplotlib inline
 import matplotlib.pyplot as plt
 import numpy as np
 plt.style.use('seaborn-whitegrid')
@@ -30,33 +30,38 @@ plt.errorbar(x, y, yerr=dy, fmt='o', color='black',
 """Sometimes we want to show errorbars on continuous quantities.
 matplotlib has no built-in function, but can combine primitives for it
 """
-# e.g. Gaussian process regression w/ Scikit-Learn API
-from sklearn.gaussian_process import GaussianProcess
 
-# Define a model and draw data.
-def model(x): return x * np.sin(x)
-xdata = np.array([1, 3, 5, 6, 8])
-ydata = model(xdata)
+# NOTE: The sklearn.gaussian_process package no longer has GaussianProcess.
+#       It contains GaussianProcessRegressor and GaussianProcessClassifier.
+#       Neither of which share kwargs we're specifying.
+
+# e.g. Gaussian process regression w/ Scikit-Learn API
+# from sklearn.gaussian_process import GaussianProcess
+
+# # Define a model and draw data.
+# def model(x): return x * np.sin(x)
+# xdata = np.array([1, 3, 5, 6, 8])
+# ydata = model(xdata)
 
 # Compute the Gaussian process fit.
-gp = GaussianProcess(corr='cubic', theta0=1e-2, thetaL=1e-4,
-                     thetaU=1E-1, random_start=100)
-gp.fit(xdata[:, np.newaxis], ydata)
+# gp = GaussianProcess(corr='cubic', theta0=1e-2, thetaL=1e-4,
+#                      thetaU=1E-1, random_start=100)
+# gp.fit(xdata[:, np.newaxis], ydata)
 
-xfit = np.linspace(0, 10, 1000)
-yfit, MSE = gp.predict(xfit[:, np.newaxis], eval_MSE=True)
-dyfit = 2 * np.sqrt(MSE)    # 2*sigma ~ 95% confidence region
+# xfit = np.linspace(0, 10, 1000)
+# yfit, MSE = gp.predict(xfit[:, np.newaxis], eval_MSE=True)
+# dyfit = 2 * np.sqrt(MSE)    # 2*sigma ~ 95% confidence region
 
 # To compute continuous error, we would not use plt.errorbar, as we don't want
 # 1000 points with 1000 errorbars.
 
 # Instead, use plt.fill_between() with a light color to visualize it
 # Visualize the result:
-plt.plot(xdata, ydata, 'or')
-plt.plot(xfit, yfit, '-', color='gray')
+# plt.plot(xdata, ydata, 'or')
+# plt.plot(xfit, yfit, '-', color='gray')
 
-plt.fill_between(xfit, yfit - dyfit, yfit + dyfit, color='gray', alpha=0.2)
-plt.xlim(0, 10)
+# plt.fill_between(xfit, yfit - dyfit, yfit + dyfit, color='gray', alpha=0.2)
+# plt.xlim(0, 10)
 
 # For more options, see plt.fill_between() docstring.
 
