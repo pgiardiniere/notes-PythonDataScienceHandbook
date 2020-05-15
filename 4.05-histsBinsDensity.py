85d269cc5d97fe14f93c0249f81a8c05eee3e8ad
pgiardiniere <pgiardiniere@gmail.com>
2020-05-15 14:58:53 -0400

    revisions and ipynb

--- 4.05-histsBinsDensity.py
+++ 4.05-histsBinsDensity.py
@@ -7,7 +7,7 @@ data = np.random.randn(1000)
 plt.hist(data)
 
 # Pass additional args to plt.hist() to customize output
-plt.hist(data, bins=30, normed=True, alpha=0.5, histtype='stepfilled',
+plt.hist(data, bins=30, density=True, alpha=0.5, histtype='stepfilled',
          color='steelblue', edgecolor='none')
 
 # This approach is more granular, and capable of multiple distributions
@@ -15,7 +15,7 @@ x1 = np.random.normal(0, 0.8, 1000)
 x2 = np.random.normal(-2, 1, 1000)
 x3 = np.random.normal(3, 2, 1000)
 
-kwargs = dict(histtype='stepfilled', alpha=0.3, normed=True, bins=40)
+kwargs = dict(histtype='stepfilled', alpha=0.3, density=True, bins=40)
 
 plt.hist(x1, **kwargs)
 plt.hist(x2, **kwargs)
